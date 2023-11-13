12ef5780e22af3554360b6945242014ded00009c
Fabricio <fabriciolak12@gmail.com>
2023-11-13 13:54:12 -0300

    chore: add support to custom url path

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,7 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseURL = env.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(path, baseURL).toString()
+  const url = new URL('/api'.concat(path), baseURL).toString()
 
   return fetch(url, init)
 }
