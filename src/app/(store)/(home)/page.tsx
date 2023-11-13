439723ad533abc0e46aeccb9444687e7d4f0d81e
Fabricio <fabriciolak12@gmail.com>
2023-11-13 01:35:23 -0300

    feat: made home and product component

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,49 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+import React from 'react'
+import { Product } from '@/app/components/product'
 
+export default async function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="grid max-h-[780px] grid-cols-9 grid-rows-6 gap-6">
+      <Product href="/">
+        <Product.Image
+          src="/camiseta-dowhile-2022.png"
+          alt="camiseta-dowhile-2022"
+          width={920}
+          height={920}
+        />
+
+        <Product.Info productName="Camiseta Dowhile 2022" productPrice={89} />
+      </Product>
+
+      <Product className="col-span-3 row-span-3" href="/">
+        <Product.Image
+          src="/moletom-ia-p-devs.png"
+          alt="moletom-ia-p-devs"
+          width={920}
+          height={920}
+        />
+
+        <Product.Info
+          productName="Camiseta Dowhile 2022"
+          productPrice={129}
+          className="bottom-10 right-10"
+        />
+      </Product>
+
+      <Product className="col-span-3 row-span-3" href="/">
+        <Product.Image
+          src="/moletom-never-stop-learning.png"
+          alt="moletom-never-stop-learning"
+          width={920}
+          height={920}
+        />
+
+        <Product.Info
+          productName="Camiseta Dowhile 2022"
+          productPrice={129}
+          className="bottom-10 right-10"
+        />
+      </Product>
+    </div>
   )
 }
---
+++ src/app/components/product.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import NextImage, { type ImageProps as NextImageProps } from 'next/image'
+import Link, { type LinkProps } from 'next/link'
+import { tw } from '../utils/tw-merge'
+
+type ProductProps = LinkProps &
+  React.HtmlHTMLAttributes<HTMLAnchorElement> & {
+    children: React.ReactNode
+  }
+
+type ImageProps = NextImageProps
+
+interface InfoProps extends React.ComponentProps<'div'> {
+  productName: string
+  productPrice: number
+}
+
+export function Product({ href, className, children, ...props }: ProductProps) {
+  return (
+    <Link
+      href={href}
+      className={tw(
+        'relative group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Link>
+  )
+}
+
+function Image({ src, alt, width, height, ...props }: ImageProps) {
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      className="group-hover:scale-105 transition-transform duration-500"
+      width={width}
+      height={height}
+      quality={100}
+      priority
+      {...props}
+    />
+  )
+}
+
+function Info({ productName, productPrice, className }: InfoProps) {
+  return (
+    <div
+      className={tw(
+        'absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280x] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5',
+        className,
+      )}
+    >
+      <span className="text-sm truncate">{productName}</span>
+      <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+        R$ {productPrice}
+      </span>
+    </div>
+  )
+}
+
+Product.displayName = 'Product'
+Product.Image = Image
+Product.Info = Info
